f211e1b239a1674444056bbddceddc4719f584ec
KnocknoteOhashi <knocknote.ohashi@gmail.com>
2022-12-10 20:26:11 +0900

    commit

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -19,7 +19,6 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // DirectionMove();
         ClickMove();
     }
     void DirectionMove()
